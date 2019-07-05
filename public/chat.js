40d78a55fb136e5d4ecd3c8a9386dc5388491835
Sachin Kumar <sac9798@gmail.com>
2019-07-05 22:31:57 +0530

    added heroku start script

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 $(function()
 {
-    var socket=io.connect("http://localhost:3000")
+    var socket=io.connect("http://localhost:8080")
 
     var message = $("#message")
 	var username = $("#username")
